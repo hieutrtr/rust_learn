7eea0fda230e335b2c5fba38a8e456e61a606b78
Hieu Tran <hieutrantrung.it@gmail.com>
2021-05-08 18:49:53 +0700

    inferred data type and print all of them

---
+++ data_types/inferred_type.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let name = "Hieu";
+    let age = 30;
+    let childs = ["Cao Nho", "Khoai Sun"];
+    let salary = 7.5;
+
+    println!("Hi {} ! \nThere's your info:\nName: {}\nAge: {}\nYour childs: {:?}\nsalary: {}", name, name, age, childs, salary);
+}
