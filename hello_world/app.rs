107a1517a3dd33e4658e3af1c4d7ea4bf50cbdbc
Hieu Tran <hieutrantrung.it@gmail.com>
2021-05-08 18:44:47 +0700

    comment

--- hello_world/app.rs
+++ hello_world/app.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello you");
+    let name = "Hieu";
+    /*
+    I comment something about my hello-world program here !
+     */
+    println!("wanna say hello to you {} !", name);
 }
